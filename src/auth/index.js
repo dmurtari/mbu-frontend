f0a521d88a68072c3c31e8e14f35c456a037caf3
Domenic Murtari <domenic.murtari@gmail.com>
2016-10-04 18:20:12 -0600

    Auth service

---
+++ src/auth/index.js
@@ -0,0 +1,53 @@
+import {router} from '../main';
+
+const API_URL = 'http://localhost:3000/api/';
+const LOGIN_URL = API_URL + 'authenticate';
+const SIGNUP_URL = API_URL + 'signup';
+
+export default {
+  user: {
+    authenticated: false
+  },
+  login(context, creds, redirect) {
+    context.$http.post(LOGIN_URL, creds, (data) => {
+      localStorage.setItem('token', data.token);
+      this.user.authenticated = true;
+
+      if(redirect) {
+        router.go(redirect);
+      }
+    }).error((err) => {
+      context.error = err;
+    });
+  },
+  signup(context, creds, redirect) {
+    context.$http.post(SIGNUP_URL, creds, (data) => {
+      localStorage.setItem('token', data.token);
+      this.user.authenticated = true;
+
+      if(redirect) {
+        router.go(redirect);
+      }
+    }).error((err) => {
+      context.error = err;
+    });
+  },
+  logout() {
+    localStorage.removeItem('token');
+    this.user.authenticated = false;
+  },
+  checkAuth() {
+    var jwt = localStorage.getItem('token');
+    if(jwt) {
+      this.user.authenticated = true;
+    }
+    else {
+      this.user.authenticated = false;      
+    }
+  },
+  getAuthHeader() {
+    return {
+      'Authorization': 'Bearer ' + localStorage.getItem('token')
+    };
+  }
+};
