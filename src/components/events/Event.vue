8d7443fe04b7535bf1afe9f9d881ee09348ed960
Domenic Murtari <domenic.murtari@gmail.com>
2016-10-27 20:17:49 -0600

    Registration dates on event display

--- src/components/events/Event.vue
+++ src/components/events/Event.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="panel panel-default">
-    <div class="panel-headering">{{ event.semester }} {{ event.year }}</div>
+    <div class="panel-heading">
+      <h4>{{ event.semester }} {{ event.year }}</h4>
+    </div>
+    <div class="panel-body">
+      <div class="row">
+        <div class="col-md-4">
+          <strong>Date:</strong> {{ event.date }}
+        </div>
+        <div class="col-md-4">
+          <strong>Registration Open:</strong> {{ event.registration_open }}
+        </div>
+        <div class="col-md-4">
+          <strong>Registration Close:</strong> {{ event.registration_close }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
