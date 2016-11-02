a2c55b64a0f0101330688123efc3aeecb4ebe594
Domenic Murtari <domenic.murtari@gmail.com>
2016-11-02 17:46:43 -0600

    Populate initial values when editing event

--- src/components/events/Edit.vue
+++ src/components/events/Edit.vue
@@ -9,28 +9,24 @@
       <div class="form-group col-sm-4 col-xs-6">
         <label for="eventDateInput">Date</label>
         <masked-input mask="99/99/9999"
-                      placeholder="mm/dd/yyyy"
                       v-model="eventUpdate.date">
       </div>
       <div class="form-group col-sm-4 col-xs-6">
         <label for="eventPriceInput">Registration Fee</label>
         <div class="input-group">
           <div class="input-group-addon">$</div>
-          <input type="number"
-                 class="form-control"
-                 v-model="eventUpdate.price">
+          <masked-input mask="99.99"
+                        v-model="eventUpdate.price">
         </div>
       </div>
       <div class="form-group col-sm-4 col-xs-6">
         <label for="eventOpenInput">Registration Opens</label>
         <masked-input mask="99/99/9999"
-                      placeholder="mm/dd/yyyy"
                       v-model="eventUpdate.registration_open">
       </div>
       <div class="form-group col-sm-4 col-xs-6">
         <label for="eventCloseInput">Registration Closes</label>
         <masked-input mask="99/99/9999"
-                      placeholder="mm/dd/yyyy"
                       v-model="eventUpdate.registration_close">
       </div>
       <div class="text-center submit-group">
@@ -68,6 +64,14 @@ export default {
     updateEvent() {
       return;
     }
+  },
+  mounted() {
+    this.eventUpdate.date = moment(this.event.date).format(dateFormat);
+    this.eventUpdate.registration_open = 
+      moment(this.event.registration_open).format(dateFormat);
+    this.eventUpdate.registration_close = 
+      moment(this.event.registration_close).format(dateFormat);
+    this.eventUpdate.price = this.event.price;
   }
 }
 </script>
