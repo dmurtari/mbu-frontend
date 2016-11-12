366dac01856274d608f1b2a3c9d15029214f9902
Domenic Murtari <domenic.murtari@gmail.com>
2016-11-11 18:42:51 -0700

    Refactored to use detail page for profile

---
+++ src/components/users/CoordinatorDetail.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    profile: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- src/components/users/Profile.vue
+++ src/components/users/Profile.vue
@@ -1,5 +1,24 @@
 <template>
 <div>
   <h3>Your Profile</h3>
+  <coordinator-detail v-if="profile.role === 'coordinator' "
+                      :profile="profile"></coordinator-detail>
 </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+import CoordinatorDetail from './CoordinatorDetail.vue';
+
+export default {
+  computed: {
+    ...mapGetters([
+      'profile'
+    ])
+  },
+  components: {
+    'coordinator-detail': CoordinatorDetail
+  }
+}
+</script>
