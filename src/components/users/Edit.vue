e6d1f760198e3e8a16fb8730903fceaf633f6a8e
Domenic Murtari <domenic.murtari@gmail.com>
2016-11-24 14:23:20 +0900

    Added profile edit file

---
+++ src/components/users/Edit.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <h4>Editing Profile</h4>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    profile: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
