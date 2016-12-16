8f34a5d6064c117ff31b6d329175b87af18ab61f
Domenic Murtari <domenic.murtari@gmail.com>
2016-12-16 08:18:22 -0700

    Fixed indent

--- src/components/events/Edit.vue
+++ src/components/events/Edit.vue
@@ -1,68 +1,68 @@
 <template>
 <div>
   <div class="alert alert-danger" v-if="error">
-      <p>
-        {{ error }}
-      </p>
-    </div>
-    <form>
-      <div class="form-group col-sm-6 col-xs-6">
-        <label for="eventDateInput">Date</label>
-        <masked-input mask="99/99/9999"
-                      v-model="eventUpdate.date">
-      </div>
-      <div class="form-group col-sm-6 col-xs-6">
-        <label for="eventPriceInput">Registration Fee</label>
-        <div class="input-group">
-          <div class="input-group-addon">$</div>
-          <masked-input mask="99.99"
-                        v-model="eventUpdate.price">
-        </div>
-      </div>
-      <div class="form-group col-sm-6 col-xs-6">
-        <label for="eventOpenInput">Registration Opens</label>
-        <masked-input mask="99/99/9999"
-                      v-model="eventUpdate.registration_open">
-      </div>
-      <div class="form-group col-sm-6 col-xs-6">
-        <label for="eventCloseInput">Registration Closes</label>
-        <masked-input mask="99/99/9999"
-                      v-model="eventUpdate.registration_close">
-      </div>
-      <div v-if="!showDeleteConfirmation">
-        <div class="col-sm-6 col-sm-push-6">
-          <div class="pull-right">
-            <button class="btn btn-primary"
-                    type="submit" 
-                    @click.prevent="updateEvent()">Update Event</button>
-            <button class="btn btn-default" 
-                    @click.prevent="close()">Cancel</button>
-          </div>
-        </div>
-        <div class="col-sm-6 col-sm-pull-6">
-          <button class="btn btn-danger"
-                  @click.prevent="showDeleteConfirm()">Delete Event</button>
-          <button class="btn btn-info"
-                  @click.prevent="setAsCurrent()">Set as the Current Event</button>
-        </div>
-      </div>   
-      <confirm-delete v-if="showDeleteConfirmation"
-                      class="container-fluid"
-                      :match-text="this.semesterAndYear"
-                      :placeholder="'Summer 2008'"
-                      @deleteSuccess="deleteEvent()"
-                      @close="hideDeleteConfirm()">
-        <span slot="header">
-          Do you really want to delete this event? This cannot be undone!
-        </span>
-        <span slot="help-text">
-          Enter the semester and year to confirm deletion. <b>This action cannot
-          be undone, and will also remove all data associated with this event, 
-          including registrations and completion records.</b>
-        </span>
-      </confirm-delete>
-    </form>
+    <p>
+      {{ error }}
+    </p>
   </div>
+  <form>
+    <div class="form-group col-sm-6 col-xs-6">
+      <label for="eventDateInput">Date</label>
+      <masked-input mask="99/99/9999"
+                    v-model="eventUpdate.date">
+    </div>
+    <div class="form-group col-sm-6 col-xs-6">
+      <label for="eventPriceInput">Registration Fee</label>
+      <div class="input-group">
+        <div class="input-group-addon">$</div>
+        <masked-input mask="99.99"
+                      v-model="eventUpdate.price">
+      </div>
+    </div>
+    <div class="form-group col-sm-6 col-xs-6">
+      <label for="eventOpenInput">Registration Opens</label>
+      <masked-input mask="99/99/9999"
+                    v-model="eventUpdate.registration_open">
+    </div>
+    <div class="form-group col-sm-6 col-xs-6">
+      <label for="eventCloseInput">Registration Closes</label>
+      <masked-input mask="99/99/9999"
+                    v-model="eventUpdate.registration_close">
+    </div>
+    <div v-if="!showDeleteConfirmation">
+      <div class="col-sm-6 col-sm-push-6">
+        <div class="pull-right">
+          <button class="btn btn-primary"
+                  type="submit" 
+                  @click.prevent="updateEvent()">Update Event</button>
+          <button class="btn btn-default" 
+                  @click.prevent="close()">Cancel</button>
+        </div>
+      </div>
+      <div class="col-sm-6 col-sm-pull-6">
+        <button class="btn btn-danger"
+                @click.prevent="showDeleteConfirm()">Delete Event</button>
+        <button class="btn btn-info"
+                @click.prevent="setAsCurrent()">Set as the Current Event</button>
+      </div>
+    </div>   
+    <confirm-delete v-if="showDeleteConfirmation"
+                    class="container-fluid"
+                    :match-text="this.semesterAndYear"
+                    :placeholder="'Summer 2008'"
+                    @deleteSuccess="deleteEvent()"
+                    @close="hideDeleteConfirm()">
+      <span slot="header">
+        Do you really want to delete this event? This cannot be undone!
+      </span>
+      <span slot="help-text">
+        Enter the semester and year to confirm deletion. <b>This action cannot
+        be undone, and will also remove all data associated with this event, 
+        including registrations and completion records.</b>
+      </span>
+    </confirm-delete>
+  </form>
+</div>
 </template>
 
 <script>
