d695eaa7d15a7c0f9db68c00f13e765801275f5c
Domenic Murtari <domenic.murtari@gmail.com>
2016-12-16 08:15:36 -0700

    Inline dropdown component

--- src/components.js
+++ src/components.js
@@ -1,7 +1,9 @@
 import MaskedInput from './components/shared/MaskedInput.vue';
 import ConfirmDelete from './components/shared/ConfirmDelete.vue';
+import InlineDropdown from './components/shared/InlineDropdown.vue';
 
 export default function(Vue) {
   Vue.component('masked-input', MaskedInput);
   Vue.component('confirm-delete', ConfirmDelete);
+  Vue.component('inline-dropdown', InlineDropdown);
 }
