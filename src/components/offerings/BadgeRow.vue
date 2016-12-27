79da1a28f149125c4448c9cc00d203c3db51d617
Domenic Murtari <domenic.murtari@gmail.com>
2016-12-27 14:41:26 -0700

    Separated offering editing to own state

--- src/components/offerings/BadgeRow.vue
+++ src/components/offerings/BadgeRow.vue
@@ -1,82 +1,34 @@
 <template>
 <div class="badge-row row">
   <b class="col-md-2">{{ badge.name }}</b>
-  <form class="form col-md-10 row" v-if="!removing">
-    <div class="form-group col-md-2">
-      <label for="offering-periods" class="sr-only">Periods</label>
-      <input type="text"
-              class="form-control"
-              id="offering-periods"
-              v-model.lazy="editablePeriods"
-              placeholder="Periods"
-              :disabled="!offered && !creating">
-    </div>
-    <div class="form-group col-md-3">
-      <label for="offering-duration" class="sr-only">Duration</label>
-      <div class="input-group">
-        <masked-input mask="9"
-                      id="offering-duration"
-                      type="Number"
-                      placeholder="Duration"
-                      v-model="offering.duration"
-                      :disabled="!offered && !creating"></masked-input>
-        <span class="input-group-addon">{{ durationLabel }}</span>
-      </div>
-    </div>
-    <div class="form-group col-md-2">
-      <label for="offering-price" class="sr-only">Price</label>
-      <div class="input-group">
-        <span class="input-group-addon">$</span>
-        <masked-input mask="99.99"
-                      id="offering-price"
-                      type="Number"
-                      placeholder="Price"
-                      v-model="offering.price"
-                      :disabled="!offered && !creating">
-      </div>
-    </div>
-    <div class="form-group col-md-5">
-      <span v-if="!creating">
-        <button v-if="offered"
-                @click.prevent="saveOffering()"
-                class="btn btn-primary">Save Offering</button>
-        <button v-if="offered" 
-                @click.prevent="toggleRemove()"
-                class="btn btn-danger">Remove This Offering</button>
-        <button v-if="!offered"
-                @click.prevent="toggleCreate()"
-                class="btn btn-success">Offer This Badge</button>
-      </span>
-      <span v-if="creating">
-        <button @click.prevent="createOffering()"
-                class="btn btn-success">Finish Adding</button>
-        <button @click.prevent="toggleCreate()"
-                class="btn btn-default">Cancel Adding</button>
-      </span>
-    </div>
-  </form>
-  <confirm-delete v-if="removing"
-                  :match-text="this.badge.name"
-                  class="col-md-10"
-                  :placeholder="'Badge Name'"
-                  @deleteSuccess="deleteOffering()"
-                  @close="toggleRemove()">
-    <span slot="header">
-      Do you really want to remove this offering? This cannot be undone, and will
-      likely break existing registration records!
+  <span v-if="offered && !editing" class="col-md-10 offering-row">
+    <span class="col-md-4 offering-detail"><b>Periods offered: </b>{{ periods }}</span>
+    <span class="col-md-4 offering-detail"><b>Duration: </b>{{ badge.duration + ' ' + durationLabel }}</span>
+    <span class="col-md-3 offering-detail"><b>Price: </b>${{ badge.price }}</span>
+    <span class="col-md-1">
+      <button class="btn btn-default"
+              v-if="isAdmin"
+              @click="toggleEdit()"
+              data-toggle="tooltip" 
+              data-placement="top" 
+              title="Edit">
+        <span class="glyphicon glyphicon-edit" aria-label="Edit"></span>
+      </button>
     </span>
-    <span slot="help-text">
-      Enter the full name of this badge with correct capitalization to confirm
-      that you wish to remove this offering. <b>This action cannot be undone, and 
-      will delete all associated completion records and badge requests! Adding
-      this badge as an offering again will not restore previous records and 
-      requests!</b>
-    </span>
-  </confirm-delete>
+  </span>
+  <edit-offering class="col-md-10" 
+                 :badge="badge"
+                 :eventId="eventId"
+                 v-if="editing"
+                 @cancel="toggleEdit()"></edit-offering>
 </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
+import Edit from './Edit.vue';
+
 export default {
   props: {
     badge: {
@@ -89,62 +41,34 @@ export default {
   },
   data() {
     return {
-      offering: {
-        periods: [],
-        duration: 1,
-        price: 0
-      },
       creating: false,
-      removing: false,
-      saving: false,
+      editing: false,
     };
   },
-  watch: {
-    badge() {
-      this.offering.periods = this.badge.periods;
-      this.offering.duration = this.badge.duration;
-      this.offering.price = this.badge.price;
-    }
-  },
   computed: {
+    ...mapGetters([
+      'isAdmin'
+    ]),
     durationLabel() {
-      return Number(this.offering.duration) == 1 ? 'period ' : 'periods';
-    },
-    editablePeriods: {
-      get() {
-        return _.join(_.sortBy(this.offering.periods), ', ');
-      },
-      set(newPeriods) {
-        this.offering.periods = _.map(_.split(newPeriods, ',', 3), (period) => {
-          return Number(_.trim(period));
-        });
-      }
+      return Number(this.badge.duration) == 1 ? 'period ' : 'periods';
     },
     offered() {
       return !_.isEmpty(this.badge.periods);
+    },
+    periods() {
+      return _.join(_.sortBy(this.badge.periods), ', ');
     }
   },
   methods: {
-    createOffering() {
-      console.log('Creating');
-    },
-    deleteOffering() {
-      console.log('Removing');
-    },
     toggleCreate() {
       this.creating = !this.creating;
     },
-    toggleRemove() {
-      this.removing = !this.removing;
-    },
-    saveOffering() {
-      console.log('Saving');
+    toggleEdit() {
+      this.editing = !this.editing;
     }
   },
-  mounted() {
-    this.offering.periods = this.badge.periods;
-    this.offering.duration = this.badge.duration;
-    this.offering.price = this.badge.price;
+  components: {
+    'edit-offering': Edit
   }
 }
 </script>
@@ -158,8 +82,12 @@ export default {
   align-items: center;
 }
 
-.form-group {
-  margin-top: 1em;
-  margin-bottom: 1em;
+.offering-row {
+  display: flex;
+  align-items: center;
+}
+
+.offering-detail {
+  white-space: nowrap;
 }
 </style>
---
+++ src/components/offerings/Edit.vue
@@ -0,0 +1,149 @@
+<template>
+<div>
+  <form class="form-inline" v-if="!removing">
+    <div class="edit-badge">
+      <div class="form-group form-item">
+        <label for="offering-periods">Periods:</label>
+        <input type="text"
+              class="form-control"
+              id="offering-periods"
+              v-model.lazy="editablePeriods"
+              placeholder="Periods">
+      </div>
+      <div class="form-group form-item">
+        <label for="offering-duration">Duration:</label>
+        <div class="input-group">
+          <select class="form-control" 
+                  id="offering-duration"
+                  v-model="offering.duration">
+            <option value="1">1 period</option>
+            <option value="2">2 periods</option>
+            <option value="3">3 periods</option>
+          </select>
+        </div>
+      </div>
+      <div class="form-group form-item">
+        <label for="offering-price">Price:</label>
+        <masked-input mask="99.99"
+                      class="input-group"
+                      id="offering-price"
+                      type="Number"
+                      placeholder="Price"
+                      v-model="offering.price">
+      </div>
+      <div class="form-group form-item">
+        <button class="btn btn-primary"
+                @click.prevent="saveOffering()">
+          <span class="glyphicon glyphicon-save" aria-label="Save"></span>
+        </button>
+        <button class="btn btn-default"
+                @click.prevent="toggleEdit()">
+          <span class="glyphicon glyphicon-remove" aria-label="Cancel"></span>
+        </button>
+        <button class="btn btn-danger"
+                @click.prevent="toggleRemove()">
+          <span class="glyphicon glyphicon-trash" aria-label="Remove"></span>
+        </button>
+      </div>
+    </div>
+  </form>
+  <confirm-delete v-if="removing"
+                  :match-text="this.badge.name"
+                  :placeholder="'Badge Name'"
+                  @deleteSuccess="deleteOffering()"
+                  @close="toggleRemove()">
+    <span slot="header">
+      Do you really want to remove this offering? This cannot be undone, and will
+      likely break existing registration records!
+    </span>
+    <span slot="help-text">
+      Enter the full name of this badge with correct capitalization to confirm
+      that you wish to remove this offering. <b>This action cannot be undone, and 
+      will delete all associated completion records and badge requests! Adding
+      this badge as an offering again will not restore previous records and 
+      requests!</b>
+    </span>
+  </confirm-delete>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    badge: {
+      type: Object,
+      required: true
+    },
+    eventId: {
+      required: true
+    }
+  },
+  data() {
+    return {
+      offering: {
+        periods: [],
+        duration: 1,
+        price: '0.00'
+      },
+      removing: false,
+      saving: false,
+    };
+  },
+  watch: {
+    badge() {
+      this.offering.periods = this.badge.periods;
+      this.offering.duration = this.badge.duration;
+      this.offering.price = this.badge.price;
+    }
+  },
+  computed: {
+    editablePeriods: {
+      get() {
+        return _.join(_.sortBy(this.offering.periods), ', ');
+      },
+      set(newPeriods) {
+        this.offering.periods = _.map(_.split(newPeriods, ',', 3), (period) => {
+          return Number(_.trim(period));
+        });
+      }
+    },
+    offered() {
+      return !_.isEmpty(this.badge.periods);
+    }
+  },
+  methods: {
+    deleteOffering() {
+      console.log('Removing');
+    },
+    toggleRemove() {
+      this.removing = !this.removing;
+    },
+    toggleEdit() {
+      this.$emit('cancel');
+    },
+    saveOffering() {
+      console.log('Saving');
+    }
+  },
+  mounted() {
+    this.offering.periods = this.badge.periods;
+    this.offering.duration = this.badge.duration;
+    this.offering.price = this.badge.price;
+  }
+}
+</script>
+
+<style lang="sass">
+.edit-badge {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-flow: row wrap;
+  flex: 1 1 20%;
+
+  .form-group {
+    margin-top: 1em;
+    margin-bottom: 1em;
+  }
+}
+</style>
