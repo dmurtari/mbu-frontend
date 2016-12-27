5f5cfb28681aaff3c06556056157f1021cdb0553
Domenic Murtari <domenic.murtari@gmail.com>
2016-12-27 10:32:59 -0700

    Offerings getter

--- src/store/modules/offerings.js
+++ src/store/modules/offerings.js
@@ -1,15 +1,46 @@
 import axios from 'axios';
+import _ from 'lodash';
 
 import * as types from '../mutation-types';
 import URLS from '../../urls';
 
-const state = {};
+const state = {
+  event: {},
+  offerings: []
+};
 
-const getters = {};
+const getters = {
+  offerings(state) {
+    return {
+      eventId: event,
+      offerings: offerings
+    };
+  }
+};
 
-const mutations = {};
+const mutations = {
+  [types.GET_OFFERINGS] (state, event) {
+    state.offerings = _.map(event.offerings, 'details');
+    state.event = event.id;
+  }
+};
 
-const actions = {};
+const actions = {  
+  getOfferings({ commit }, eventId) {
+    return new Promise((resolve, reject) => {
+      axios.get(URLS.EVENTS_URL + '?id=' + eventId)
+        .then((response) => {
+          console.log('Received event', response.data[0]);
+          commit(types.GET_OFFERINGS, response.data[0]);
+          resolve();
+        })
+        .catch(() => {
+          console.log('Failed to get event offerings');
+          reject();
+        });
+    }); 
+  }
+};
 
 export default {
   state,
