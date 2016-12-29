0fc501e2ac809dac3aa36af8d142fd1020d4d6a8
Domenic Murtari <domenic.murtari@gmail.com>
2016-12-29 11:48:25 -0700

    Basic list of offerings

--- src/components/events/Detail.vue
+++ src/components/events/Detail.vue
@@ -1,26 +1,59 @@
 <template>
-  <div class="row">
-    <div class="col-md-3">
-      <strong>Date:</strong> {{ event.date | longDate }}
+  <div>
+    <div class="row">
+      <div class="col-md-3">
+        <strong>Date:</strong> {{ event.date | longDate }}
+      </div>
+      <div class="col-md-3">
+        <strong>Registration Fee:</strong> ${{ event.price }}
+      </div>
+      <div class="col-md-3">
+        <strong>Registration Open:</strong> {{ event.registration_open | longDate}}
+      </div>
+      <div class="col-md-3">
+        <strong>Registration Close:</strong> {{ event.registration_close | longDate}}
+      </div>
     </div>
-    <div class="col-md-3">
-      <strong>Registration Fee:</strong> ${{ event.price }}
+    <br>
+    <h4>Badges offered at this event:</h4>
+    <div v-if="orderedOfferings.length > 0">
+      <div v-for="offering in orderedOfferings">
+        {{ offering.name }}
+        <span v-if="Number(offering.details.price) > 0">
+          (${{offering.details.price}})
+        </span>
+      </div>
     </div>
-    <div class="col-md-3">
-      <strong>Registration Open:</strong> {{ event.registration_open | longDate}}
-    </div>
-    <div class="col-md-3">
-      <strong>Registration Close:</strong> {{ event.registration_close | longDate}}
+    <div v-else>
+      No badges are listed as available at this event.
+      <span v-if="isAdmin">
+        Offer badges for this event at the
+        <router-link to="/offerings">offerings page</router-link>.
+      </span>
+      <span v-if="!isAdmin">
+        Please check with the event coordinators if you feel this is not correct.
+      </span>
     </div>
   </div>
 </template>
 
 <script>
+import _ from 'lodash';
+import { mapGetters } from 'vuex';
+
 export default {
   props: {
     event: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'isAdmin'
+    ]),
+    orderedOfferings() {
+      return _.orderBy(this.event.offerings, 'name');
     }
   }
 }  
