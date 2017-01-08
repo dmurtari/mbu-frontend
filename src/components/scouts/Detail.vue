faa724706845a97b317aa2103d96a9ce60e00fa1
Domenic Murtari <domenic.murtari@gmail.com>
2017-01-08 10:59:58 -0700

    Change to using rows

--- src/components/scouts/Detail.vue
+++ src/components/scouts/Detail.vue
@@ -1,36 +1,32 @@
 <template>
-  <div>
-    <div class="row">
-      <div class="col-lg-12">
+  <div class="container-fluid">
+      <div class="row">
         <strong>Added On:</strong> {{ scout.created_at | longDate }}
       </div>
-      <div class="col-lg-12">
+      <div class="row">
         <strong>Birthday:</strong> {{ scout.birthday | longDate }}
       </div>
-      <div class="col-lg-12">
+      <div class="row">
         <strong>Age:</strong> {{ scout.age }}
       </div>
-      <div class="col-lg-12">
+      <div class="row">
         <strong>Troop:</strong> {{ scout.troop }}
       </div>
-    </div>
-    <div class="row col-lg-12" v-if="scout.notes">
+    <div class="row" v-if="scout.notes">
       <strong>Notes:</strong> {{ scout.notes }}
     </div>
     <br><br>
-    <div class="row col-lg-12">
+    <div class="row">
       <h5>Emergency Contact Information (Please keep this information up to date):</h5>
     </div>
     <div class="row">
-      <div class="col-lg-4">
-        <strong>Name:</strong> {{ scout.emergency_name }}
-      </div>
-      <div class="col-lg-4">
-        <strong>Relationship to Scout:</strong> {{ scout.emergency_relation }}
-      </div>
-      <div class="col-lg-4">
-        <strong>Contact Number:</strong> {{ scout.emergency_phone }}
-      </div>
+      <strong>Name:</strong> {{ scout.emergency_name }}
+    </div>
+    <div class="row">
+      <strong>Relationship to Scout:</strong> {{ scout.emergency_relation }}
+    </div>
+    <div class="row">
+      <strong>Contact Number:</strong> {{ scout.emergency_phone }}
     </div>
   </div>
 </template>
