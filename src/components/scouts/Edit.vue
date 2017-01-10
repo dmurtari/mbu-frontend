11d4b04f943dd7d609677834300b5504a8424a73
Domenic Murtari <domenic.murtari@gmail.com>
2017-01-09 17:27:02 -0700

    Edit scout view

---
+++ src/components/scouts/Edit.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Editing scout
+  </div>
+</template>
--- src/components/scouts/Scout.vue
+++ src/components/scouts/Scout.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="panel panel-default">
     <div class="panel-heading">
-      <h4>{{ scout.firstname }} {{ scout.lastname }}</h4>
+      <div class="btn-group pull-right">
+        <button class="btn btn-default"
+                @click="toggleEdit()"
+                data-toggle="tooltip" 
+                data-placement="top" 
+                title="Edit">
+          <span class="glyphicon glyphicon-edit" aria-label="Edit"></span>
+        </button>
+      </div>
+      <h4>
+        {{ scout.firstname }} {{ scout.lastname }}
+      </h4>
     </div>
     <div class="panel-body">
-      <scout-detail :scout="scout"></scout-detail>
+      <scout-detail v-if="!editing" :scout="scout"></scout-detail>
+      <scout-edit v-if="editing" :scout="scout"></scout-edit>
     </div>
   </div>
 </template>
 
 <script>
 import Detail from './Detail.vue';
+import Edit from './Edit.vue';
 
 export default {
   props: {
@@ -19,8 +32,19 @@ export default {
       type: Object
     }
   },
+  data() {
+    return {
+      editing: false
+    };
+  },
+  methods: {
+    toggleEdit() {
+      this.editing = !this.editing;
+    }
+  },
   components: {
-    'scout-detail': Detail
+    'scout-detail': Detail,
+    'scout-edit': Edit
   }
 }
 </script>
