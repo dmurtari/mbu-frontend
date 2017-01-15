a660c27e83bfe1f77b9cfbf9b8f301dca5a117c9
Domenic Murtari <domenic.murtari@gmail.com>
2017-01-15 13:10:58 -0700

    Profile sidebar

--- src/components/users/profile/Profile.vue
+++ src/components/users/profile/Profile.vue
@@ -1,63 +1,65 @@
 <template>
-<div class="container-fluid">
-  <div class="page-header">
-    <h3>Your Profile</h3>
-  </div>
-  <aside class="col-sm-4" data-spy="affix" data-offset-top="60">
-    <h5>Jump to a section:</h5>
-    <div class="list-group">
-      <a href="#general-profile" 
-         @click="showDisplay()" 
-         class="list-group-item">My Profile</a>
-      <a href="#coordinator-details" 
-         @click="showDisplay()" 
-         class="list-group-item">Details</a>
-    </div>
-    <div class="sidebar-buttons list-group">
-      <h5>Make changes to your account:</h5>
-      <div class="btn-block">
-        <button type="button" 
-                class="list-group-item"
-                @click="toggleEdit()">
-          {{ state === 'editProfile' ? 'Cancel Edits' : 'Edit Profile' }}
-        </button>
-        <button type="button" 
-                class="list-group-item"
-                @click="toggleEditPassword()">
-          {{ state === 'editPassword' ? 'Cancel Password Change' : 'Change Password' }}
-        </button>
+  <section class="section">
+    <h1 class="title">Your Profile</h1>
+    <h3 class="subtitle">View and make changes to your profile</h3>
+    <div class="columns">
+      <div class="column is-3">
+        <aside class="menu">
+          <p class="menu-label">Jump to a section</p>
+          <ul class="menu-list">
+            <li>
+              <a href="#general-profile" 
+                @click="showDisplay()">My Profile</a>
+            </li>
+            <li>
+              <a href="#coordinator-details" 
+                @click="showDisplay()">Details</a>
+            </li>
+          </ul>
+          <p class="menu-label">Make changes to your account</p>
+          <ul class="menu-list">
+            <li>
+              <a @click="toggleEdit()">
+                {{ state === 'editProfile' ? 'Cancel Edits' : 'Edit Profile' }}
+              </a>
+            </li>
+            <li>
+              <a @click="toggleEditPassword()">
+                {{ state === 'editPassword' ? 'Cancel Password Change' : 'Change Password' }}
+              </a>
+            </li>
+            <li>
+              <a @click="toggleDeleteAccount()">
+                {{ state === 'deleteAccount' ? 'Don\'t Delete' : 'Delete My Account' }}
+              </a>
+            </li>
+          </ul>
+        </aside>
       </div>
-      <br>
-      <button type="button" 
-              class="list-group-item list-group-item-danger"
-              @click="toggleDeleteAccount()">
-        {{ state === 'deleteAccount' ? 'Don\'t Delete' : 'Delete My Account' }}
-      </button>
+      <div class="column">
+        <div v-if="state === 'display'">
+          <general-profile id="general-profile"
+                           class="general-profile" :profile="profile"></general-profile>
+          <coordinator-detail v-if="profile.role === 'coordinator' "
+                              id="coordinator-details"></coordinator-detail>
+        </div>
+        <div v-if="state === 'editProfile'">
+          <edit-profile :profile="profile"
+                        @toggle="toggleEdit()"></edit-profile>
+        </div>
+        <div v-if="state === 'editPassword'">
+          <edit-password :id="profile.id"
+                         @toggle="toggleEditPassword()"></edit-password>
+        </div>
+        <div v-if="state === 'deleteAccount'">
+          <delete-account :firstname="profile.firstname"
+                          :lastname="profile.lastname"
+                          :role="profile.role"
+                          :id="profile.id"
+                          @toggle="toggleDeleteAccount()"></delete-account>
+      </div>
     </div>
-  </aside>
-  <section class="col-sm-7 col-sm-offset-1">
-    <div v-if="state === 'display'">
-      <general-profile id="general-profile"
-                       class="general-profile" :profile="profile"></general-profile>
-      <coordinator-detail v-if="profile.role === 'coordinator' "
-                          id="coordinator-details"></coordinator-detail>
-    </div>
-    <div v-if="state === 'editProfile'">
-      <edit-profile :profile="profile"
-                    @toggle="toggleEdit()"></edit-profile>
-    </div>
-    <div v-if="state === 'editPassword'">
-      <edit-password :id="profile.id"
-                     @toggle="toggleEditPassword()"></edit-password>
-    </div>
-    <div v-if="state === 'deleteAccount'">
-      <delete-account :firstname="profile.firstname"
-                      :lastname="profile.lastname"
-                      :role="profile.role"
-                      :id="profile.id"
-                      @toggle="toggleDeleteAccount()"></delete-account>
   </section>
-</div>
 </template>
 
 <script>
@@ -130,18 +132,3 @@ export default {
   }
 }
 </script>
-
-<style scoped lang="sass">
-.general-profile {
-  padding-bottom: 1em;
-}
-
-.sidebar-buttons {
-  margin-top: 1em;
-  margin-bottom: 3em;
-}
-
-button:focus, input[type="button"]:focus, .list-group-item {
-  outline: none;
-}
-</style>
