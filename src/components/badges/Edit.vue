81b30fdb3928efb0be126940f59d9ea312ce3756
Domenic Murtari <domenic.murtari@gmail.com>
2017-01-16 20:13:19 -0700

    Badge edits

--- src/components/badges/Edit.vue
+++ src/components/badges/Edit.vue
@@ -1,64 +1,65 @@
 <template>
-<form>
-  <div class="row">
-    <div class="form-group col-sm-4 col-xs-6">
-      <label for="badge-edit-name">Name</label>
-      <input type="text"
-              class="form-control"
-              id="badge-edit-name"
-              placeholder="New Badge"
-              v-model="badgeUpdate.name">
-    </div>
-    <div class="form-group col-sm-8 col-xs-6">
-      <label for="badge-edit-notes">Notes</label>
-      <input type="text"
-              class="form-control"
-              id="badge-edit-notes"
-              placeholder="Notes about this badge"
-              v-model="badgeUpdate.notes">
-    </div>
-  </div>
-  <div class="row">
-    <div class="form-group col-xs-12">
-      <label for="badge-edit-description">Description</label>
-      <textarea class="form-control"
-                id="badge-edit-description"
-                rows="5"
-                placeholder="Description of this badge"
-                v-model="badgeUpdate.description"></textarea>
-    </div>
-  </div>
-  <div v-if="!showDeleteConfirmation">
-    <div class="pull-right">
-      <button class="btn btn-primary" 
+  <div>
+    <form v-if="!showDeleteConfirmation">
+      <div class="columns is-multiline">
+        <div class="content column is-4">
+          <label class="label" for="badge-edit-name">Name</label>
+          <input type="text"
+                class="input"
+                id="badge-edit-name"
+                placeholder="New Badge"
+                @blur="$v.badgeUpdate.name.$touch"
+                 :class="{ 'is-danger': $v.badgeUpdate.name.$error }"
+                v-model="badgeUpdate.name">
+          <span class="help is-danger" v-if="$v.badgeUpdate.name.$error">
+            The name of the merit badge is required
+          </span>
+        </div>
+        <div class="content column is-8">
+          <label class="label" for="badge-edit-notes">Notes</label>
+          <input type="text"
+                class="input"
+                id="badge-edit-notes"
+                placeholder="Notes about this badge"
+                v-model="badgeUpdate.notes">
+        </div>
+        <div class="content column">
+          <label class="label" for="badge-edit-description">Description</label>
+          <textarea class="textarea"
+                    id="badge-edit-description"
+                    rows="5"
+                    placeholder="Description of this badge"
+                    v-model="badgeUpdate.description"></textarea>
+        </div>
+      </div>
+      <button class="button is-primary" 
               @click.prevent="updateBadge()">Save Changes</button>
-      <button class="btn btn-default" 
+      <button class="button" 
               @click.prevent="close()">Cancel</button>
-    </div>
-    <div class="pull-left">
-      <button class="btn btn-danger"
+      <button class="button is-danger is-pulled-right"
               @click.prevent="showDeleteConfirm()">Delete Badge</button>
-    </div>
+    </form>
+    <confirm-delete v-if="showDeleteConfirmation"
+                    :match-text="this.badge.name"
+                    :placeholder="'Badge Name'"
+                    @deleteSuccess="deleteBadge()"
+                    @close="hideDeleteConfirm()">
+      <span slot="header">
+        Do you really want to delete this badge? This cannot be undone, and will
+        likely break existing registration records!
+      </span>
+      <span slot="help-text">
+        Enter the full name of this badge with correct capitalization to confirm
+        that you wish to delete this badge. <b>This action cannot be undone, and 
+        will delete all associated completion records and badge requests!</b>
+      </span>
+    </confirm-delete>
   </div>
-  <confirm-delete v-if="showDeleteConfirmation"
-                  :match-text="this.badge.name"
-                  :placeholder="'Badge Name'"
-                  @deleteSuccess="deleteBadge()"
-                  @close="hideDeleteConfirm()">
-    <span slot="header">
-      Do you really want to delete this badge? This cannot be undone!, and will
-      likely break existing registration records!
-    </span>
-    <span slot="help-text">
-      Enter the full name of this badge with correct capitalization to confirm
-      that you wish to delete this badge. <b>This action cannot be undone, and 
-      will delete all associated completion records and badge requests!</b>
-    </span>
-  </confirm-delete>
-</form>
 </template>
 
 <script>
+import { required } from 'vuelidate/lib/validators';
+
 export default {
   props: {
     badge: {
@@ -122,6 +123,11 @@ export default {
     this.badgeUpdate.name = this.badge.name;
     this.badgeUpdate.description = this.badge.description;
     this.badgeUpdate.notes = this.badge.notes;
+  },
+  validations: {
+    badgeUpdate: {
+      name: { required }
+    }
   }
 }
 </script>
