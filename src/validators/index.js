e9c5ca5a86e14ac5e4dcc5e221699bff3076481d
Domenic Murtari <domenic.murtari@gmail.com>
2017-01-24 17:33:19 -0700

    Basic phone number validator

--- src/validators/index.js
+++ src/validators/index.js
@@ -2,10 +2,12 @@ import afterDate from './afterDate';
 import beforeDate from './beforeDate';
 import betweenDate from './betweenDate';
 import date from './date';
+import phone from './phone';
 
 export {
   afterDate,
   beforeDate,
   betweenDate,
-  date
+  date,
+  phone
 }
