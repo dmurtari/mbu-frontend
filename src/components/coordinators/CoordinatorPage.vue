6c124a8ccab73c8c1cc630c42b84b8c3a5c309cd
Domenic Murtari <domenic.murtari@gmail.com>
2017-01-31 17:52:20 -0700

    Load data before display

--- src/components/coordinators/CoordinatorPage.vue
+++ src/components/coordinators/CoordinatorPage.vue
@@ -11,37 +11,52 @@
         <aside class="menu">
           <p class="menu-label">Jump to a Section</p>
           <ul class="menu-list">
-            <a href="#" @click="display('scouts')">Manage Scouts</a>
-            <a href="#" @click="display('registrations')">Event Registrations</a>
+            <router-link to="/coordinator/scouts">Manage Scouts</router-link>
+            <router-link to="/coordinator/registrations">Event Registrations</router-link>
           </ul>
         </aside>
       </div>
       <div class="column">
-        <scouts v-if="displayed === 'scouts'"></scouts>
-        <registrations v-if="displayed === 'registrations'"></registrations>
+        <div class="notification is-danger" v-if="error">
+          <p>{{ error }}</p>
+        </div>
+        <router-view></router-view>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Registrations from './registrations/List.vue';
-import Scouts from './scouts/List.vue';
+import store from '../../store'
 
 export default {
   data() {
     return {
-      displayed: 'scouts'
+      error: '',
+      loading: true
     };
   },
-  methods: {
-    display(newDisplay) {
-      this.displayed = newDisplay;
-    }
-  },
-  components: {
-    'registrations': Registrations,
-    'scouts': Scouts,
+  beforeRouteEnter(to, from, next) {
+    store.dispatch('getProfile')
+      .then(() => {
+        return Promise.all([
+          store.dispatch('getRegistrations', store.state.authentication.profile.id),
+          store.dispatch('getEvents'),
+          store.dispatch('getBadges')
+        ])
+      })
+      .then(() => {
+        next(vm => {
+          console.log(vm)
+          vm.loading = false;
+        });
+      })
+      .catch(() => {
+        next(vm => {
+          vm.loading = false;
+          vm.error = 'Unable to fetch data from server. Please try again.';
+        })
+      })
   }
 }
 </script>
