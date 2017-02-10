a04236d2033860ba54c15c79ea0dcdf8465cf346
Domenic Murtari <domenic.murtari@gmail.com>
2017-02-09 18:01:06 -0700

    Edit and delete registrations

---
+++ src/components/coordinators/registrations/Edit.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="is-fullwidth">
+    <h5 class="title is-5">Modify Registration for {{ scout.fullname }}</h5>
+    <div class="notification is-danger"
+         v-if="error">
+      <p>
+        {{ error }}
+      </p>
+    </div>
+    <div v-if="!showDeleteConfirmation" class="columns is-multiline">
+      <template v-for="(preference, index) in preferences">
+        <div class="column is-6 is-4-widescreen">
+          <label class="label"
+                  :for="'registration-rank' + index">
+            {{ index + 1 | ordinalSuffix }}&nbsp;choice:
+          </label>
+          <div class="control">
+            <span class="select">
+              <select class="input"
+                      :id="'registration-rank' + index"
+                      @blur="$v.preferences.$each[index].$touch"
+                      :class="{ 'is-danger': $v.preferences.$each[index].$error }"
+                      v-model="preference.offering">
+                <option v-for="option in offerings"
+                        :value="option.details.id">
+                  {{ option.name }}
+                </option>
+              </select>
+            </span>
+            <span class="help is-danger"
+                  v-if="$v.preferences.$each[index].$error">
+              Please select a {{ index + 1 | ordinalSuffix }}&nbsp;choice badge
+            </span>
+          </div>
+        </div>
+      </template>
+      <div class="column is-12">
+        <button class="button is-primary"
+                :disabled="$v.$invalid"
+                :class="{ 'is-disabled is-loading': saving }"
+                @click="save()">Save Changes</button>
+        <button class="button"
+                :class="{ 'is-disabled': saving }"
+                @click="cancel()">Cancel</button>
+        <button class="button is-danger is-pulled-right"
+                @click.prevent="toggleDeleteConfirmation()">Unregister</button>
+      </div>
+    </div>
+    <confirm-delete v-if="showDeleteConfirmation"
+                    class="container-fluid"
+                    :match-text="this.event.semester + ' ' + this.event.year"
+                    :placeholder="this.event.semester + ' ' + this.event.year"
+                    :cancelText="'Don\'t Remove'"
+                    :confirmText="'Remove Registration'"
+                    @deleteSuccess="deleteRegistration()"
+                    @close="toggleDeleteConfirmation()">
+      <span slot="header">
+        Do you really want to unregister {{ scout.fullname }} from this event?
+      </span>
+      <span slot="help-text">
+        Enter the event's semester and year to confirm that you wish to remove
+        {{ scout.fullname }}'s registration for MBU
+        {{ this.event.semester + ' ' + this.event.year }}. You can re-register
+        {{ scout.fullname }} for this event at any time <b>before the event's
+        registration closes ({{ event.registration_close | longDate }}).</b>
+      </span>
+    </confirm-delete>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash';
+
+import { mapGetters } from 'vuex';
+import { required } from 'vuelidate/lib/validators';
+
+export default {
+  props: {
+    scout: {
+      type: Object,
+      required: true
+    },
+    event: {
+      type: Object,
+      required: true
+    },
+    registration: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      preferences: [],
+      saving: false,
+      error: '',
+      showDeleteConfirmation: false
+    };
+  },
+  computed: {
+    offerings() {
+      return _.orderBy(this.event.offerings, 'name');
+    }
+  },
+  methods: {
+    cancel() {
+      this.$emit('cancel');
+    },
+    deleteRegistration() {
+      return this.$store.dispatch('deleteRegistration', {
+        scoutId: this.scout.id,
+        eventId: this.event.id
+      })
+        .then(() => {
+          this.error = '';
+          this.$emit('saved');
+        })
+        .catch((err) => {
+          this.error = 'Failed to delete registration. Please refresh and try again.';
+        })
+    },
+    save() {
+      if (!this.uniqueOfferings()) {
+        this.error = 'Merit Badge requests cannot be duplicates.';
+        return;
+        }
+
+      this.saving = true;
+      return this.$store.dispatch('setPreferences', {
+        scoutId: this.scout.id,
+        registrationId: this.registration.details.id,
+        preferences: this.preferences
+      })
+        .then(() => {
+          this.saving = false;
+          this.error = '';
+          this.$emit('saved');
+        })
+        .catch((err) => {
+          this.saving = false;
+          this.error = 'Failed to save preferences. Please refresh and try again';
+        });
+    },
+    uniqueOfferings() {
+      let offerings = _.map(this.preferences, 'offering');
+      return _.uniq(offerings).length === offerings.length;
+    },
+    toggleDeleteConfirmation() {
+      this.showDeleteConfirmation = !this.showDeleteConfirmation;
+    },
+  },
+  mounted() {
+    _.forEach(this.registration.preferences, (preference) => {
+      this.preferences.push({
+        offering: preference.offering_id,
+        rank: preference.details.rank
+      });
+    });
+
+    for (var i = this.preferences.length + 1; i <= 6; i++) {
+      this.preferences.push({
+        rank: i,
+        offering: ''
+      })
+    }
+  },
+  validations: {
+    preferences: {
+      $each: {
+        offering: { required }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass" scope>
+  .is-fullwidth {
+    width: 100%;
+  }
+</style>
--- src/components/coordinators/registrations/RegistrationRow.vue
+++ src/components/coordinators/registrations/RegistrationRow.vue
@@ -1,52 +1,80 @@
 <template>
-  <div class="registration-row is-flex-tablet columns is-multiline">
-    <template v-if="state === 'displaying'">
-      <b class="column is-2">{{ scout.fullname }}</b>
-      <loader v-if="loading" :color="'lightgray'"></loader>
-      <template v-if="registration && !loading">
-        <div class="column auto">
-          <p>
-            <b>Projected Fee: </b>${{ projectedCost }}
-          </p>
-          <p>
-            <b>Badge Preferences: </b>{{ preferences }}
-          </p>
-        </div>
-        <div class="column is-1">
-          <button class="button"
-                  @click="toggleState('editing')"
-                  @cancel="toggleState()">
-            <span class="fa fa-edit" aria-label="Modify"></span>
+  <div class="registration-row ">
+    <div class="notification is-danger" v-if="error">
+      <p>
+        {{ error }}
+      </p>
+    </div>
+    <div class="is-flex-tablet columns is-multiline">
+      <template v-if="state === 'displaying'">
+        <b class="column is-2 is-hidden-mobile">{{ scout.fullname }}</b>
+        <h5 class="column is-2 title is-5 is-hidden-tablet">{{ scout.fullname }}</h5>
+        <loader v-if="loading" :color="'lightgray'"></loader>
+        <template v-if="registration && !loading">
+          <div class="column auto">
+            <p>
+              <b>Projected Fee: </b>{{ projectedCost | currency }}
+              <ul class="price-list">
+                <li>Event Fee: {{ event.price | currency }}</li>
+                <li v-for="preference in preferences" v-if="preference.price !== '0.00'">
+                  {{ preference.name }}: {{ preference.price | currency }}
+                </li>
+              </ul>
+            </p>
+            <br>
+            <p>
+              <b>Merit Badge Preferences: </b>
+              <ol class="preference-list">
+                <li v-for="preference in preferences">
+                  {{ preference.name }}
+                  <span v-if="preference.price !== '0.00'">
+                    ({{ preference.price | currency }})
+                  </span>
+                </li>
+              </ol>
+            </p>
+          </div>
+          <div class="column is-1">
+            <button class="button"
+                    @click="toggleState('editing')"
+                    @cancel="toggleState()"
+                    data-balloon="Modify Registration"
+                    data-balloon-pos="left"
+                    :disabled="!registrationOpen">
+              <span class="fa fa-edit is-hidden-mobile" aria-label="Modify"></span>
+              <span class="is-hidden-tablet">Modify Registration</span>
+            </button>
+          </div>
+        </template>
+        <template v-if="!registration">
+          <button class="button is-link register-button"
+                  v-if="registrationOpen"
+                  @click="toggleState('creating')">
+            Register for {{ event.semester + ' ' + event.year }}
           </button>
-        </div>
+          <p v-if="!registrationOpen">
+            Sorry, registration for this event is closed.
+          </p>
+        </template>
       </template>
-      <template v-if="!registration">
-        <button class="button is-link"
-                v-if="isRegistrationOpen"
-                @click="toggleState('creating')">
-          Register for {{ event.semester + ' ' + event.year }}
-        </button>
-        <p v-if="!isRegistrationOpen">
-          Sorry, registration for this event is closed.
-        </p>
-      </template>
-    </template>
-    <create-registration v-if="state === 'creating'"
-                         :scout="scout"
-                         :event="event"
-                         @cancel="toggleState()"
-                         @created="toggleState()"></create-registration>
-    <edit-registration v-if="state === 'editing'"
-                       :scout="scout"
-                       :registration="registration"
-                       @cancel="toggleState()"></edit-registration>
+      <create-registration v-if="state === 'creating'"
+                          :scout="scout"
+                          :event="event"
+                          @cancel="toggleState()"
+                          @created="toggleState()"></create-registration>
+      <edit-registration v-if="state === 'editing'"
+                        :scout="scout"
+                        :event="event"
+                        :registration="registration"
+                        @cancel="toggleState()"
+                        @saved="toggleState()"></edit-registration>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 import _ from 'lodash';
-import moment from 'moment';
 
 import CreateRegistration from './Create.vue';
 import EditRegistration from './Edit.vue';
@@ -57,7 +85,12 @@ export default {
       type: Object,
       required: true
     },
-    eventId: {
+    event: {
+      type: Object,
+      required: true
+    },
+    registrationOpen: {
+      type: Boolean,
       required: true
     }
   },
@@ -69,7 +102,7 @@ export default {
     };
   },
   watch: {
-    eventId() {
+    event() {
       this.state = 'displaying';
     },
     registration() {
@@ -80,41 +113,38 @@ export default {
           registrationId: this.registration.details.id
         })
           .then(() => {
+            this.error = '';
             this.loading = false;
           })
           .catch(() => {
             this.loading = false;
             this.error = 'Failed to load registration details. Please refresh and try again';
           })
+      } else {
+        this.state = 'displaying';
       }
     }
   },
   computed: {
-    ...mapGetters([
-      'allEvents',
-      'badgeIdsAndNames'
-    ]),
-    event() {
-      return _.find(this.allEvents, { 'id': this.eventId });
-    },
-    isRegistrationOpen() {
-      if (this.event) {
-        return moment().isBetween(this.event.registration_open,
-                                  this.event.registration_close,
-                                  null,
-                                  '[]');
-      }
+    offerings() {
+      return this.event.offerings;
     },
     preferences() {
-      return _.join(_.map(this.registration.preferences, (preference) => {
-        return _.find(this.badgeIdsAndNames, { id: preference.badge_id }).name;
-      }), ', ');
+      return _.map(this.registration.preferences, (preference) => {
+        let offering = _.find(this.offerings, { id: preference.badge_id });
+        return {
+          name: offering.name,
+          price: offering.details.price
+        }
+      });
     },
     projectedCost() {
-      return this.event.price;
+      return _.reduce(this.preferences, (sum, preference) => {
+        return sum + Number(preference.price);
+      }, Number(this.event.price));
     },
     registration() {
-      return _.find(this.scout.registrations, { 'event_id': this.eventId });
+      return _.find(this.scout.registrations, { 'event_id': this.event.id });
     }
   },
   methods: {
@@ -131,9 +161,24 @@ export default {
 
 <style lang="sass" scoped>
   .registration-row {
-    padding-top: 1em;
-    padding-bottom: 1em;
+    padding: 2rem 1rem;
     border-bottom: 1px lightgray solid;
     align-items: center;
+  }
+
+  .price-list {
+    padding-top: .5rem;
+    padding-left: 1rem;
+  }
+
+  .preference-list {
+    padding-top: .5rem;
+    padding-left: 2rem;
+    columns: 2;
+    column-gap: 3rem;
+  }
+
+  .register-button {
+    height: auto;
   }
 </style>
