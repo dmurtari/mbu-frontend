43c2368ac028fc19d0418ee3d56f1a5246e28cff
Domenic Murtari <domenic.murtari@gmail.com>
2017-03-07 18:28:48 -0700

    Use registration component in admin view

--- src/components/administration/attendance/AttendanceRow.vue
+++ src/components/administration/attendance/AttendanceRow.vue
@@ -9,88 +9,27 @@
       </button>
     </h5>
     <div v-if="state === 'displaying'">
-      <div class="attendance-section columns">
-        <p class="column is-6">
-          <b>Projected Cost of Attendance: </b>{{ projectedCost | currency }}
-          <ul class="itemized-list">
-            <li>Event Fee: {{ event.price | currency }}</li>
-            <li>Merit Badge Fees: {{ preferenceCosts | currency }}</li>
-            <li>Purchases: {{ purchaseCosts | currency }}</li>
-          </ul>
+      <scout-registration :event="event"
+                          :preferences="registrationPreferences"
+                          :purchases="registrationPurchases"
+                          :assignments="registrationAssignments">
+        <p slot="assignment-notification">
+          {{ scout.fullname }} has not yet been assigned any merit badges for
+          MBU {{ event.semester }} {{ event.year }}.
+          <br>
+          <a class="is-link"
+              @click.prevent="toggleState('assigning')">
+            Assign Merit Badges
+          </a>
         </p>
-        <p class="column is-6">
-          <b>Actual Cost of Attendance: </b>{{ actualCost | currency }}
-          <ul class="itemized-list">
-            <li>Event Fee: {{ event.price | currency }}</li>
-            <li>Merit Badge Fees: {{ assignmentCosts | currency }}</li>
-            <li>Purchases: {{ purchaseCosts | currency }}</li>
-          </ul>
-        </p>
-      </div>
-      <div class="attendance-preferences attendance-section">
-        <b>Merit Badge Preferences:</b>
-        <ul class="itemized-list attendance-list">
-          <li v-for="preference in preferences"
-              :key="preference.id"
-              v-if="preferences.length > 0">
-            {{ preference.details.rank }}: {{ preference.badge.name }}
-            <span v-if="preference.price !== '0.00'">
-              ({{ preference.price | currency }})
-            </span>
-          </li>
-          <div v-else class="notification">
-            This scout does not have any preferences listed for this event.
-          </div>
-        </ul>
-      </div>
-      <div class="attendance-purchases attendance-section" v-if="purchases.length > 0">
-        <b>Purchases:</b>
-        <ul class="itemized-list">
-          <li v-for="purchase in purchases" :key="purchase.id">
-            {{ purchase.item }}
-            <span v-if="purchase.details.size">
-              (Size: {{ purchase.details.size | capitalize }})
-            </span>
-            {{ purchase.price | currency }} &times; {{ purchase.details.quantity }} =
-            {{ purchase.price * purchase.details.quantity | currency }}
-          </li>
-        </ul>
-      </div>
-      <div class="attendance-assignments attendance-section">
-        <b>Assignments:</b>
-        <div class="attendance-section" v-if="assignments.length < 1">
-          <div class="notification">
-            {{ scout.fullname }} has not yet been assigned any merit badges for
-            MBU {{ event.semester }} {{ event.year }}.
-            <br>
-            <a class="is-link"
-               @click.prevent="toggleState('assigning')">
-              Assign Merit Badges
-            </a>
-          </div>
-        </div>
-        <div v-else>
-          <ul class="itemized-list">
-            <li v-for="(assignment, index) in assignmentList" :key="index">
-              Period {{ index + 1 }}:
-              <span v-if="!assignment">Unassigned</span>
-              <span v-else>
-                {{ assignment.badge.name }}
-                <span v-if="assignment.price !== '0.00'">
-                  ({{ assignment.price | currency }})
-                </span>
-              </span>
-            </li>
-          </ul>
-        </div>
-      </div>
+      </scout-registration>
     </div>
     <assignment-edit v-if="state === 'assigning'"
-                       :scout="scout"
-                       :event="event"
-                       :preferences="preferences"
-                       :registration="registration"
-                       @done="toggleState('displaying')"></assignment-edit>
+                     :scout="scout"
+                     :event="event"
+                     :preferences="preferences"
+                     :registration="registration"
+                     @done="toggleState('displaying')"></assignment-edit>
   </div>
 </template>
 
@@ -98,6 +37,7 @@
 import _ from 'lodash';
 
 import AssignmentEdit from './AssignmentEdit.vue';
+import ScoutRegistration from '../../scouts/ScoutRegistration.vue';
 
 export default {
   props: {
@@ -116,21 +56,7 @@ export default {
     };
   },
   computed: {
-    scout() { return this.registration.scout },
-    preferences() {
-      return _.orderBy(this.registration.preferences, ['details.rank']);
-    },
     assignments() { return this.registration.assignments },
-    purchases() { return this.registration.purchases },
-    actualCost() {
-      return Number(this.event.price) + Number(this.assignmentCosts)
-             + Number(this.purchaseCosts);
-    },
-    assignmentCosts() {
-      return _.reduce(this.assignments, (sum, assignment) => {
-        return sum + Number(assignment.price);
-      }, 0);
-    },
     assignmentList() {
       let result = [null, null, null];
 
@@ -142,19 +68,37 @@ export default {
 
       return result;
     },
-    preferenceCosts() {
-      return _.reduce(this.preferences, (sum, preference) => {
-        return sum + Number(preference.price);
-      }, 0);
+    scout() { return this.registration.scout },
+    preferences() {
+      return _.orderBy(this.registration.preferences, ['details.rank']);
     },
-    projectedCost() {
-      return Number(this.event.price) + Number(this.preferenceCosts)
-             + Number(this.purchaseCosts);
+    purchases() { return this.registration.purchases },
+    registrationAssignments() {
+      return _.map(_.omitBy(this.assignmentList, _.isNil), (assignment) => {
+        return {
+          name: assignment.badge.name,
+          price: assignment.price
+        }
+      });
     },
-    purchaseCosts() {
-      return _.reduce(this.purchases, (sum, purchase) => {
-        return sum + (Number(purchase.price) * Number(purchase.details.quantity));
-      }, 0);
+    registrationPreferences() {
+      return _.map(this.preferences, (preference) => {
+        return {
+          name: preference.badge.name,
+          price: preference.price
+        };
+      });
+    },
+    registrationPurchases() {
+      return _.map(this.purchases, (purchase) => {
+        return {
+          price: purchase.price,
+          size: purchase.details.size,
+          item: purchase.item,
+          quantity: purchase.details.quantity,
+          id: purchase.id
+        };
+      });
     }
   },
   watch: {
@@ -168,7 +112,8 @@ export default {
     }
   },
   components: {
-    AssignmentEdit
+    AssignmentEdit,
+    ScoutRegistration
   }
 }
 </script>
@@ -177,19 +122,5 @@ export default {
   .attendance-row {
     padding: 2rem 1rem;
     border-bottom: 1px lightgray solid;
-  }
-
-  .attendance-section {
-    margin-top: 1rem;
-  }
-
-  .attendance-list {
-    columns: 2;
-    column-gap: 3rem;
-  }
-
-  .itemized-list {
-    margin-top: .5rem;
-    padding-left: 1rem;
   }
 </style>
--- src/components/scouts/ScoutRegistration.vue
+++ src/components/scouts/ScoutRegistration.vue
@@ -1,27 +1,42 @@
 <template>
   <div>
-    <p>
-      <b>Projected Cost of Attendance: </b>{{ projectedCost | currency }}
-      <ul class="itemized-list">
-        <li>Event Fee: {{ event.price | currency }}</li>
-        <li>Merit Badge Fees: {{ preferenceCosts | currency }}</li>
-        <li>Purchases: {{ purchaseCosts | currency }}</li>
-      </ul>
-    </p>
-    <br>
-    <p>
+    <div class="columns">
+      <p class="column is-6">
+        <b>Projected Cost of Attendance: </b>{{ projectedCost | currency }}
+        <ul class="itemized-list">
+          <li>Event Fee: {{ event.price | currency }}</li>
+          <li>Merit Badge Fees: {{ preferenceCosts | currency }}</li>
+          <li>Purchases: {{ purchaseCosts | currency }}</li>
+        </ul>
+      </p>
+      <p class="column is-6">
+        <b>Actual Cost of Attendance: </b>{{ actualCost | currency }}
+        <ul class="itemized-list">
+          <li>Event Fee: {{ event.price | currency }}</li>
+          <li>Merit Badge Fees: {{ assignmentCosts | currency }}</li>
+          <li>Purchases: {{ purchaseCosts | currency }}</li>
+        </ul>
+      </p>
+    </div>
+    <div class="registration-section">
       <b>Merit Badge Preferences:</b>
-      <ol class="preference-list">
-        <li v-for="preference in preferences" :key="preference.name">
-          {{ preference.name }}
-          <span v-if="preference.price !== '0.00'">
-            ({{ preference.price | currency }})
-          </span>
-        </li>
-      </ol>
-    </p>
-    <p v-if="purchases.length > 0">
-      <br>
+      <div class="preference-list" v-if="preferences.length > 0">
+        <ol>
+          <li v-for="preference in preferences" :key="preference.name">
+            {{ preference.name }}
+            <span v-if="preference.price !== '0.00'">
+              ({{ preference.price | currency }})
+            </span>
+          </li>
+        </ol>
+      </div>
+      <div v-else class="notification">
+        <slot name="preference-notification">
+          This scout does not have any preferences listed for this event.
+        </slot>
+      </div>
+    </div>
+    <div v-if="purchases.length > 0" class="registration-section">
       <b>Purchased Items:</b>
       <ul class="itemized-list">
         <li v-for="purchase in purchases" :key="purchase.id">
@@ -33,7 +48,32 @@
           {{ purchase.price * purchase.quantity | currency }}
         </li>
       </ul>
-    </p>
+    </div>
+    <div class="registration-section">
+      <b>Assignments:</b>
+      <div class="attendance-section" v-if="assignments.length < 1">
+        <div class="notification">
+          <slot name="assignment-notification">
+            No Merit Badges have been assigned for MBU {{ event.semester }}
+            {{ event.year }}.
+          </slot>
+        </div>
+      </div>
+      <div v-else>
+        <ul class="itemized-list">
+          <li v-for="(assignment, index) in assignments" :key="index">
+            Period {{ index + 1 }}:
+            <span v-if="!assignment">Unassigned</span>
+            <span v-else>
+              {{ assignment.name }}
+              <span v-if="assignment.price !== '0.00'">
+                ({{ assignment.price | currency }})
+              </span>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -58,6 +98,15 @@ export default {
     }
   },
   computed: {
+    actualCost() {
+      return Number(this.event.price) + Number(this.assignmentCosts)
+             + Number(this.purchaseCosts);
+    },
+    assignmentCosts() {
+      return _.reduce(this.assignments, (sum, assignment) => {
+        return sum + Number(assignment.price);
+      }, 0);
+    },
     preferenceCosts() {
       return _.reduce(this.preferences, (sum, preference) => {
         return sum + Number(preference.price);
@@ -87,5 +136,13 @@ export default {
     padding-left: 2rem;
     columns: 2;
     column-gap: 3rem;
+  }
+
+  .registration-section {
+    margin-top: 1.5rem;
+  }
+
+  .notification {
+    margin-top: 1rem;
   }
 </style>
