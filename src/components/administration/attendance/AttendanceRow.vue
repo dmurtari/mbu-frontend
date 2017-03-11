92094cf6f4018d24cb7996b7ffb555f2818a926a
Domenic Murtari <domenic.murtari@gmail.com>
2017-03-11 16:05:19 -0700

    Pull registration info to mixin

--- src/components/administration/attendance/AttendanceRow.vue
+++ src/components/administration/attendance/AttendanceRow.vue
@@ -38,68 +38,13 @@ import _ from 'lodash';
 
 import AssignmentEdit from './AssignmentEdit.vue';
 import ScoutRegistration from '../../scouts/ScoutRegistration.vue';
+import RegistrationMappers from '../../../mixins/RegistrationMappers';
 
 export default {
-  props: {
-    registration: {
-      type: Object,
-      required: true
-    },
-    event: {
-      type: Object,
-      required: true
-    }
-  },
   data() {
     return {
       state: 'displaying'
     };
-  },
-  computed: {
-    assignments() { return this.registration.assignments },
-    assignmentList() {
-      let result = [null, null, null];
-
-      _.forEach(this.registration.assignments, (assignment) => {
-        return _.forEach(assignment.details.periods, (period) => {
-          result[Number(period) - 1] = assignment;
-        });
-      });
-
-      return result;
-    },
-    scout() { return this.registration.scout },
-    preferences() {
-      return _.orderBy(this.registration.preferences, ['details.rank']);
-    },
-    purchases() { return this.registration.purchases },
-    registrationAssignments() {
-      return _.map(_.omitBy(this.assignmentList, _.isNil), (assignment) => {
-        return {
-          name: assignment.badge.name,
-          price: assignment.price
-        }
-      });
-    },
-    registrationPreferences() {
-      return _.map(this.preferences, (preference) => {
-        return {
-          name: preference.badge.name,
-          price: preference.price
-        };
-      });
-    },
-    registrationPurchases() {
-      return _.map(this.purchases, (purchase) => {
-        return {
-          price: purchase.price,
-          size: purchase.details.size,
-          item: purchase.item,
-          quantity: purchase.details.quantity,
-          id: purchase.id
-        };
-      });
-    }
   },
   watch: {
     registration() {
@@ -114,7 +59,10 @@ export default {
   components: {
     AssignmentEdit,
     ScoutRegistration
-  }
+  },
+  mixins: [
+    RegistrationMappers
+  ]
 }
 </script>
 
