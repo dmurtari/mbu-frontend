0759bc2491bfba1d8e1d7f4b3378ddff96a652a7
Domenic Murtari <domenic.murtari@gmail.com>
2017-03-31 18:34:38 -0600

    Fix unit test config

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -11,12 +11,12 @@ module.exports = function (config) {
         loaders: [
           {
             test: /\.js$/,
-            loader: 'babel',
+            loader: 'babel-loader',
             exclude: /node_modules/
           },
           {
             test: /\.vue$/,
-            loader: 'vue'
+            loader: 'vue-loader'
           }
         ]
       }
