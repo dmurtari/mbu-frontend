1c7c72869d873462fd6d3df89f1692b79e0bd0ff
Domenic Murtari <domenic.murtari@gmail.com>
2017-04-21 18:37:04 -0600

    Description of validator for comma separated list

---
+++ test/unit/validators/commaSeparatedSpec.js
@@ -0,0 +1,22 @@
+import commaSeparated from '../../../src/validators/commaSeparated';
+
+describe.only('The comma separated validator', () => {
+  it('should validate a comma separated list', () => {
+    expect(commaSeparated('1, 2, 3')).to.be.true;
+    expect(commaSeparated('1,2,3')).to.be.true;
+    expect(commaSeparated('1, 2,3')).to.be.true;
+    expect(commaSeparated('1a, 2, 3b')).to.be.true;
+    expect(commaSeparated('1a, 2, a')).to.be.true;
+  });
+
+  it('should fail for other punctuation', () => {
+    expect(commaSeparated('1.2.3')).to.be.false;
+    expect(commaSeparated('1,2.3')).to.be.false;
+  });
+
+  it('should pass for empty values', () => {
+    expect(commaSeparated(null)).to.be.true;
+    expect(commaSeparated('')).to.be.true;
+    expect(commaSeparated(' ')).to.be.true;
+  });
+});
