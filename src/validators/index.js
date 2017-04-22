7b52d4ee8ccf036dead3adf69ab4471c129b57c7
Domenic Murtari <domenic.murtari@gmail.com>
2017-04-21 18:37:14 -0600

    Validator for comma separated list

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,6 +1,7 @@
 import afterDate from './afterDate';
 import beforeDate from './beforeDate';
 import betweenDate from './betweenDate';
+import commaSeparated from './commaSeparated';
 import date from './date';
 import greaterThan from './greaterThan';
 import lessThan from './lessThan';
@@ -11,6 +12,7 @@ export {
   afterDate,
   beforeDate,
   betweenDate,
+  commaSeparated,
   date,
   greaterThan,
   lessThan,
