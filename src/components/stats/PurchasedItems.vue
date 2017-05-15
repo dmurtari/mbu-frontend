f7b831a6364c6302e34214721938fe5bd08e2fcf
Domenic Murtari <domenic.murtari@gmail.com>
2017-05-14 20:39:25 -0600

    Initial work on mapping purchases

--- src/components/stats/PurchasedItems.vue
+++ src/components/stats/PurchasedItems.vue
@@ -5,11 +5,21 @@
 </template>
 
 <script>
+import _ from 'lodash';
+
 export default {
   props: {
     registrations: {
       type: Array,
       required: true
+    }
+  },
+  computed: {
+    purchases () {
+      return _.flatten(_.map(this.registrations, 'purchases'));
+    },
+    groupedPurchases () {
+      return _.groupBy(this.purchases, 'item');
     }
   }
 }
