39b6bf19901d54c3c94f608e48a6cdd7f60a90a6
Domenic Murtari <domenic.murtari@gmail.com>
2017-05-16 18:04:50 -0600

    Basic display of purchase costs

--- src/components/stats/PurchasedItems.vue
+++ src/components/stats/PurchasedItems.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
     Purchase Details:
+    <ul>
+      <li v-for="(purchases, item) in groupedPurchases" :key="item">
+        <span v-if="purchases.hasSize">
+          {{ item }}
+          <ul>
+            <li v-for="(items, size) in purchases.items" :key="size">
+              {{ size | capitalize }}
+              ({{ totalQuantity(items) }} &times; {{ items[0].price | currency }} =
+               {{ totalDue(items) | currency }})
+            </li>
+          </ul>
+        </span>
+        <span v-else>
+          {{ item }}
+          ({{ totalQuantity(purchases) }} &times; {{ purchases[0].price | currency }} =
+          {{ totalDue(purchases) | currency }})
+        </span>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -27,15 +46,31 @@ export default {
 
       _.forEach(this.purchases, (items, key) => {
         if (items[0].has_size) {
+          groupedItems[key] = {
+            hasSize: true,
+            items: []
+          };
+
           let result = _.chain(items)
             .groupBy('details.size')
             .value();
-          groupedItems[key] = result;
-          groupedItems[key]['hasSize'] = true;
+          groupedItems[key].items = result;
         }
       });
 
       return groupedItems;
+    }
+  },
+  methods: {
+    totalDue (purchases) {
+      return _.reduce(purchases, (sum, item) => {
+        return sum += (Number(item.price) * Number(item.details.quantity));
+      }, 0);
+    },
+    totalQuantity (purchases) {
+      return _.reduce(purchases, (sum, item) => {
+        return sum += Number(item.details.quantity)
+      }, 0);
     }
   }
 }
