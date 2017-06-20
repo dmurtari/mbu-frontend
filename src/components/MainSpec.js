f552fe61fbd0bbee15f46008dccc9269fb39a8bb
Domenic Murtari <domenic.murtari@gmail.com>
2017-06-19 18:21:30 -0700

    Main view spec

---
+++ src/components/MainSpec.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Main from './Main.vue';
+
+function getRenderedText (Component, propsData) {
+  const Ctor = Vue.extend(Component);
+  const vm = new Ctor({ propsData: propsData }).$mount();
+  return vm.$el.textContent;
+}
+
+describe('Main', () => {
+  it('should render the welcome text', () => {
+    expect(getRenderedText(Main)).to.contain('Welcome to MBU Online');
+  });
+});
