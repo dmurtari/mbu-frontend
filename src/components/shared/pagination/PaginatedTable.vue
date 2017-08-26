39a293af9688bbe19e169f629768d471477cd081
Domenic Murtari <domenic.murtari@gmail.com>
2017-08-25 19:29:36 -0600

    Put back original paginated table

--- src/components/shared/pagination/PaginatedTable.vue
+++ src/components/shared/pagination/PaginatedTable.vue
@@ -1,32 +1,58 @@
 <template>
   <div>
-    <paginated-items :target="target"
-                     :contents="contents"
-                     :limit="limit"
-                     :per="per"
-                     :showLinks="showLinks">
-      <paginate slot="pagination-contents"
-                :name="target"
-                :list="contents"
-                :per="per"
-                tag="table"
-                class="table is-striped paginated-table is-fullwidth"
-                ref="paginator">
-        <slot name="header"></slot>
-        <tbody>
-          <slot name="row"
-                v-for="(item, index) in paginated(target)"
-                :item="item">
-          </slot>
-        </tbody>
-      </paginate>
-    </paginated-items>
+    <nav class="pagination is-centered">
+      <paginate-links :for="target"
+                      :limit="limit"
+                      :show-step-links="showLinks"
+                      class="pagination-list"
+                      :step-links="{
+                          next: 'Next',
+                          prev: 'Previous'
+                        }"
+                      :classes="{
+                          'li.number > a, li.ellipses > a': 'pagination-link',
+                          'li.left-arrow': 'pagination-previous',
+                          'li.right-arrow': 'pagination-next'
+                        }"
+                      @change="pageChanged">
+      </paginate-links>
+    </nav>
+    <paginate :name="target"
+              :list="contents"
+              :per="per"
+              tag="table"
+              class="table is-striped paginated-table is-fullwidth"
+              ref="paginator">
+      <slot name="header"></slot>
+      <tbody>
+        <slot name="row"
+              v-for="(item, index) in paginated(target)"
+              :item="item">
+        </slot>
+      </tbody>
+    </paginate>
+    <nav class="pagination is-centered">
+      <paginate-links :for="target"
+                      :limit="limit"
+                      :show-step-links="showLinks"
+                      class="pagination-list"
+                      :step-links="{
+                          next: 'Next',
+                          prev: 'Previous'
+                        }"
+                      :classes="{
+                          'li.number > a, li.ellipses > a': 'pagination-link',
+                          'li.left-arrow': 'pagination-previous',
+                          'li.right-arrow': 'pagination-next'
+                        }"
+                      @change="pageChanged">
+      </paginate-links>
+    </nav>
   </div>
 </template>
 
 <script>
 import _ from 'lodash';
-
 export default {
   props: {
     target: {
@@ -59,7 +85,6 @@ export default {
     pageChanged (toPage) {
       let query = _.clone(this.$route.query);
       query.page = toPage;
-
       this.$router.replace({
         query: query
       });
@@ -77,24 +102,19 @@ export default {
 table.paginated-table {
   margin-top: 2rem;
   table-layout: fixed;
-
   .icon {
     font-size: 16px;
   }
-
   .sorted-column {
     background: #EEE;
   }
-
   th.sortable:hover {
     background: #EEE;
   }
-
   td {
     word-wrap: break-word;
   }
 }
-
 .pagination-list {
   li.number {
     background-color: white;
@@ -102,37 +122,30 @@ table.paginated-table {
     margin-right: 0rem;
     min-width: 0em;
     padding: 0rem;
-
     a {
       font-size: 1rem;
     }
   }
-
   .disabled {
     background: #dbdbdb;
     opacity: 0.5;
     pointer-events: none;
-
     a {
       color: #7a7a7a;
       font-size: 1rem;
     }
   }
-
   .pagination-next,
   .pagination-previous {
     width: auto;
     font-size: 1rem;
-
     .pagination-link {
       border: none;
     }
   }
-
   .pagination-previous {
     order: 0 !important;
   }
-
   li.active {
     a {
       font-size: 1rem;
