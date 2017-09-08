2957739d98f33858a17724ff98eef3dff90ec604
Domenic Murtari <domenic.murtari@gmail.com>
2017-09-07 20:28:05 -0600

    Reworked links

--- src/components/navigation/Footer.vue
+++ src/components/navigation/Footer.vue
@@ -1,24 +1,21 @@
 <template>
   <footer class="footer">
-    <div class="container">
       <div class="content has-text-centered">
         <p>
           <strong>MBU Online</strong> by Domenic Murtari
         </p>
         <p>
-          <router-link to="/about">About</router-link>
-          &nbsp;|&nbsp;
+          Source code licensed
+          <a href="https://opensource.org/licenses/MIT">MIT</a>, website licensed
+          <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Version 4</a>
+        </p>
+        <p>
           <router-link to="/privacy">Privacy Policy</router-link>
           &nbsp;|&nbsp;
           <a href="mailto:support@mbu.online">Technical Problems?</a>
+          &nbsp;|&nbsp;
+          <a href="https://github.com/dmurtari/mbu-frontend">Contribute</a>
         </p>
-        <p>
-          <a class="icon"
-             href="https://github.com/dmurtari/mbu-frontend">
-            <i class="fa fa-github"></i>
-          </a>
-        </p>
-      </div>
     </div>
   </footer>
 </template>
@@ -30,14 +27,14 @@ html {
 }
 
 body {
-  margin: 0 0 150px;
+  margin-bottom: 180px;
 }
 
 footer {
   position: absolute;
   left: 0;
   bottom: 0;
-  height: 150px;
+  height: 180px;
   width: 100%;
 
   .icon {
