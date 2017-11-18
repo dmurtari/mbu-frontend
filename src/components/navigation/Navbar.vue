9a0162be3d6a4915c931afc37a467ede1212ba04
Domenic Murtari <domenic.murtari@gmail.com>
2017-11-17 19:10:45 -0700

    Container around navbar

--- src/components/navigation/Navbar.vue
+++ src/components/navigation/Navbar.vue
@@ -1,63 +1,65 @@
 <template>
   <nav class="navbar has-shadow">
-    <div class="navbar-brand">
-      <router-link class="navbar-item"
-                   to="/"
-                   active-class="brand-active">
-        <h5 class="title is-5">
-          MBU Home
-        </h5>
-      </router-link>
-      <span class="navbar-burger"
-            :class="{ 'is-active': dropdownActive }"
-            @click.prevent="toggleDropdown()">
-        <span></span>
-        <span></span>
-        <span></span>
-      </span>
-    </div>
-    <div class="navbar-menu"
-         :class="{ 'is-active': dropdownActive }">
-      <div class="navbar-start">
+    <div class="container">
+      <div class="navbar-brand">
         <router-link class="navbar-item"
-                     to="/badges">Badges</router-link>
-        <router-link class="navbar-item"
-                     to="/events">Events</router-link>
-        <router-link class="navbar-item"
-                     v-if="isCoordinator && isApproved"
-                     to="/coordinator">Manage Troop</router-link>
-        <router-link class="navbar-item"
-                     v-if="isTeacher && isApproved"
-                     to="/teacher">Instructor Tasks</router-link>
-        <router-link class="navbar-item"
-                     v-if="isAdmin"
-                     to="/administration">Administration</router-link>
-      </div>
-      <div class="navbar-end">
-        <span class="navbar-item"
-              v-if="!isAuthenticated">
-          <div class="field is-grouped">
-            <div class="control">
-              <router-link class="button is-primary"
-                           to="/login">Login</router-link>
-            </div>
-            <div class="control">
-              <router-link class="button is-info is-outlined"
-                           to="/signup">Sign Up</router-link>
-            </div>
-          </div>
-        </span>
-        <router-link class="navbar-item"
-                     v-if="isAuthenticated"
-                     to="/profile">
-          Profile
+                     to="/"
+                     active-class="brand-active">
+          <h5 class="title is-5">
+            MBU Home
+          </h5>
         </router-link>
-        <span class="navbar-item"
-              v-if="isAuthenticated">
-          <a href="#"
-             class="button"
-             @click.prevent="logout()">Logout</a>
+        <span class="navbar-burger"
+              :class="{ 'is-active': dropdownActive }"
+              @click.prevent="toggleDropdown()">
+          <span></span>
+          <span></span>
+          <span></span>
         </span>
+      </div>
+      <div class="navbar-menu"
+           :class="{ 'is-active': dropdownActive }">
+        <div class="navbar-start">
+          <router-link class="navbar-item"
+                       to="/badges">Badges</router-link>
+          <router-link class="navbar-item"
+                       to="/events">Events</router-link>
+          <router-link class="navbar-item"
+                       v-if="isCoordinator && isApproved"
+                       to="/coordinator">Manage Troop</router-link>
+          <router-link class="navbar-item"
+                       v-if="isTeacher && isApproved"
+                       to="/teacher">Instructor Tasks</router-link>
+          <router-link class="navbar-item"
+                       v-if="isAdmin"
+                       to="/administration">Administration</router-link>
+        </div>
+        <div class="navbar-end">
+          <span class="navbar-item"
+                v-if="!isAuthenticated">
+            <div class="field is-grouped">
+              <div class="control">
+                <router-link class="button is-primary"
+                             to="/login">Login</router-link>
+              </div>
+              <div class="control">
+                <router-link class="button is-info is-outlined"
+                             to="/signup">Sign Up</router-link>
+              </div>
+            </div>
+          </span>
+          <router-link class="navbar-item"
+                       v-if="isAuthenticated"
+                       to="/profile">
+            Profile
+          </router-link>
+          <span class="navbar-item"
+                v-if="isAuthenticated">
+            <a href="#"
+               class="button"
+               @click.prevent="logout()">Logout</a>
+          </span>
+        </div>
       </div>
     </div>
   </nav>
